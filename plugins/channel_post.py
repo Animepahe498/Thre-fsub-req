196776a59b330326e54737535b4cea7960421f2b
Animepahe498 <148706595+Animepahe498@users.noreply.github.com>
2024-08-09 17:22:10 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -48,10 +48,20 @@ async def channel_post(client: Client, message: Message):
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
+    shortened_link = shorten_url(original_link)
+    
+    inline_keyboard = [
+        [
+            InlineKeyboardButton("Original Link", url=original_link)
+        ],
+        [
+            InlineKeyboardButton("Shortened Link", url=shortened_link)
+        ]
+    ]
 
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    reply_markup = InlineKeyboardMarkup(inline_keyboard)
 
-    await reply_text.edit(f"<b>Here is your link</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
+    await reply_text.edit(f"<b>Here is your link\n\Original Link: {original_link}\n\nShortened Link: {shortened_link}</b>", reply_markup=reply_markup, disable_web_page_preview = True)
 
     if not DISABLE_CHANNEL_BUTTON:
         await post_message.edit_reply_markup(reply_markup)
@@ -66,7 +76,18 @@ async def new_post(client: Client, message: Message):
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    shortened_link = shorten_url(original_link)
+    
+    inline_keyboard = [
+        [
+            InlineKeyboardButton("Original Link", url=original_link)
+        ],
+        [
+            InlineKeyboardButton("Shortened Link", url=shortened_link)
+        ]
+    ]
+
+    reply_markup = InlineKeyboardMarkup(inline_keyboard)
     try:
         await message.edit_reply_markup(reply_markup)
     except Exception as e:
